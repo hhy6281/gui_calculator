35cbcf28901962be8802684e93cc596a0f13e98a
Hee Yong <117414837+hhy6281@users.noreply.github.com>
2022-12-02 01:10:20 +0900

    새 버튼 추가 #12

--- calculator_main.py
+++ calculator_main.py
@@ -86,7 +86,6 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "pyqt 계산기"))
