5ab102aebee0056ab78942b8c129f063f3504e60
Hee Yong <117414837+hhy6281@users.noreply.github.com>
2022-12-02 01:04:41 +0900

    Update calculator_main.py

--- calculator_main.py
+++ calculator_main.py
@@ -1,156 +1,241 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-from PyQt5.QtWidgets import *
 
-class Main(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(360, 370)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(5, 150, 80, 40))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(95, 150, 80, 40))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(185, 150, 80, 40))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(185, 200, 80, 40))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(5, 200, 80, 40))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(95, 200, 80, 40))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(185, 250, 80, 40))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_8.setGeometry(QtCore.QRect(95, 300, 80, 40))
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setGeometry(QtCore.QRect(5, 300, 80, 40))
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.pushButton_10 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_10.setGeometry(QtCore.QRect(5, 250, 80, 40))
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.pushButton_11 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_11.setGeometry(QtCore.QRect(185, 300, 80, 40))
+        self.pushButton_11.setObjectName("pushButton_11")
+        self.pushButton_12 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_12.setGeometry(QtCore.QRect(95, 250, 80, 40))
+        self.pushButton_12.setObjectName("pushButton_12")
+        self.pushButton_13 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_13.setGeometry(QtCore.QRect(275, 150, 80, 40))
+        self.pushButton_13.setObjectName("pushButton_13")
+        self.pushButton_14 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_14.setGeometry(QtCore.QRect(275, 250, 80, 40))
+        self.pushButton_14.setObjectName("pushButton_14")
+        self.pushButton_15 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_15.setGeometry(QtCore.QRect(275, 200, 80, 40))
+        self.pushButton_15.setObjectName("pushButton_15")
+        self.pushButton_16 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_16.setGeometry(QtCore.QRect(275, 300, 80, 40))
+        self.pushButton_16.setObjectName("pushButton_16")
+        self.pushButton_17 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_17.setGeometry(QtCore.QRect(5, 100, 200, 40))
+        self.pushButton_17.setObjectName("pushButton_17")
+        self.pushButton_18 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_18.setGeometry(QtCore.QRect(210, 100, 145, 40))
+        self.pushButton_18.setObjectName("pushButton_18")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(5, 5, 350, 70))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(15)
+        self.label.setFont(font)
+        self.label.setStyleSheet("QLabel{ \n"
+"            border : 2px solid black;\n"
+"            background : white;\n"
+"            }")
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 360, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-    def init_ui(self):
-        main_layout = QVBoxLayout()
-
-        ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
-        layout_operation = QHBoxLayout()
-        layout_clear_equal = QHBoxLayout()
-        layout_number = QGridLayout()
-        layout_equation_solution = QFormLayout()
-
-        self.temp_number = 0
-        self.temp_operator = ""
-
-        ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Number: ")
-        self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
-
-        self.number_display = QLineEdit("")
-
-        ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        # layout_equation_solution.addRow(label_equation, self.equation)
-        # layout_equation_solution.addRow(label_solution, self.solution)
-        layout_equation_solution.addRow(self.number_display)
-
-        ### 사칙연상 버튼 생성
-        button_plus = QPushButton("+")
-        button_minus = QPushButton("-")
-        button_product = QPushButton("x")
-        button_division = QPushButton("/")
-
-        ### 사칙연산 버튼을 클릭했을 때, 각 사칙연산 부호가 수식창에 추가될 수 있도록 시그널 설정
-        button_plus.clicked.connect(lambda state, operation = "+": self.button_operation_clicked(operation))
-        button_minus.clicked.connect(lambda state, operation = "-": self.button_operation_clicked(operation))
-        button_product.clicked.connect(lambda state, operation = "*": self.button_operation_clicked(operation))
-        button_division.clicked.connect(lambda state, operation = "/": self.button_operation_clicked(operation))
-
-        ### 사칙연산 버튼을 layout_operation 레이아웃에 추가
-        layout_operation.addWidget(button_plus)
-        layout_operation.addWidget(button_minus)
-        layout_operation.addWidget(button_product)
-        layout_operation.addWidget(button_division)
-
-        ### =, clear, backspace 버튼 생성
-        button_equal = QPushButton("=")
-        button_clear = QPushButton("Clear")
-        button_backspace = QPushButton("Backspace")
-
-        ### =, clear, backspace 버튼 클릭 시 시그널 설정
-        button_equal.clicked.connect(self.button_equal_clicked)
-        button_clear.clicked.connect(self.button_clear_clicked)
-        button_backspace.clicked.connect(self.button_backspace_clicked)
-
-        ### =, clear, backspace 버튼을 layout_clear_equal 레이아웃에 추가
-        layout_clear_equal.addWidget(button_clear)
-        layout_clear_equal.addWidget(button_backspace)
-        layout_clear_equal.addWidget(button_equal)
-
-        ### 숫자 버튼 생성하고, layout_number 레이아웃에 추가
-        ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
-        number_button_dict = {}
-        for number in range(0, 10):
-            number_button_dict[number] = QPushButton(str(number))
-            number_button_dict[number].clicked.connect(lambda state, num = number:
-                                                       self.number_button_clicked(num))
-            if number >0:
-                x,y = divmod(number-1, 3)
-                layout_number.addWidget(number_button_dict[number], x, y)
-            elif number==0:
-                layout_number.addWidget(number_button_dict[number], 3, 1)
-
-        ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
-        button_dot = QPushButton(".")
-        button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
-        layout_number.addWidget(button_dot, 3, 2)
-
-        button_double_zero = QPushButton("00")
-        button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
-        layout_number.addWidget(button_double_zero, 3, 0)
-
-        ### 각 레이아웃을 main_layout 레이아웃에 추가
-        main_layout.addLayout(layout_equation_solution)
-        main_layout.addLayout(layout_operation)
-        main_layout.addLayout(layout_clear_equal)
-        main_layout.addLayout(layout_number)
-
-        self.setLayout(main_layout)
-        self.show()
-
-    #################
-    ### functions ###
-    #################
-    def number_button_clicked(self, num):
-        equation = self.equation.text()
-        equation += str(num)
-        self.equation.setText(equation)
-
-    def button_operation_clicked(self, operation):
-
-        if operation not in ["square", "root", "inverse"]:
-            self.temp_number = float(self.number_display.text())
-            self.number_display.setText("")
-
-            self.temp_operator = operation
-
-        else:
-            if operation == "square":
-                self.temp_number = float(self.number_display.text())
-                self.temp_number = self.temp_number*self.temp_number
-                self.number_display.setText(str(self.temp_number))
-                pass
-            if operation == "root":
-                pass
-            if operation == "inverse":
-                pass
-
-            self.temp_operator = ""
-            self.temp_number = 0
-        # equation = self.equation.text()
-        #equation += operation
-        # self.equation.setText(equation)
-
-    def button_equal_clicked(self):
-        # equation = self.equation.text()
-        #solution = eval(equation)
-        # self.solution.setText(str(solution))
-        temp_second_number = float(self.number_display.text())
-
-        if self.temp_operator == "+":
-            temp_result = temp_second_number + self.temp_number
-
-        self.number_display.setText(str(temp_result))
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
-    def button_clear_clicked(self):
-        self.equation.setText("")
-        self.solution.setText("")
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "pyqt 계산기"))
+        self.pushButton.setText(_translate("MainWindow", "1"))
+        self.pushButton_2.setText(_translate("MainWindow", "2"))
+        self.pushButton_3.setText(_translate("MainWindow", "3"))
+        self.pushButton_5.setText(_translate("MainWindow", "6"))
+        self.pushButton_6.setText(_translate("MainWindow", "4"))
+        self.pushButton_7.setText(_translate("MainWindow", "5"))
+        self.pushButton_4.setText(_translate("MainWindow", "9"))
+        self.pushButton_8.setText(_translate("MainWindow", "."))
+        self.pushButton_9.setText(_translate("MainWindow", "0"))
+        self.pushButton_10.setText(_translate("MainWindow", "7"))
+        self.pushButton_11.setText(_translate("MainWindow", "/"))
+        self.pushButton_12.setText(_translate("MainWindow", "8"))
+        self.pushButton_13.setText(_translate("MainWindow", "*"))
+        self.pushButton_14.setText(_translate("MainWindow", "+"))
+        self.pushButton_15.setText(_translate("MainWindow", "-"))
+        self.pushButton_16.setText(_translate("MainWindow", "="))
+        self.pushButton_17.setText(_translate("MainWindow", "Clear"))
+        self.pushButton_18.setText(_translate("MainWindow", "Del"))
+        self.label.setText(_translate("MainWindow", ""))
 
-    def button_backspace_clicked(self):
-        equation = self.equation.text()
-        equation = equation[:-1]
-        self.equation.setText(equation)
+        # adding action to each of the button
+        self.pushButton_15.clicked.connect(self.action_minus)
+        self.pushButton_16.clicked.connect(self.action_equal)
+        self.pushButton_9.clicked.connect(self.action0)
+        self.pushButton.clicked.connect(self.action1)
+        self.pushButton_2.clicked.connect(self.action2)
+        self.pushButton_3.clicked.connect(self.action3)
+        self.pushButton_6.clicked.connect(self.action4)
+        self.pushButton_7.clicked.connect(self.action5)
+        self.pushButton_5.clicked.connect(self.action6)
+        self.pushButton_10.clicked.connect(self.action7)
+        self.pushButton_12.clicked.connect(self.action8)
+        self.pushButton_4.clicked.connect(self.action9)
+        self.pushButton_11.clicked.connect(self.action_div)
+        self.pushButton_13.clicked.connect(self.action_mul)
+        self.pushButton_14.clicked.connect(self.action_plus)
+        self.pushButton_8.clicked.connect(self.action_point)
+        self.pushButton_17.clicked.connect(self.action_clear)
+        self.pushButton_18.clicked.connect(self.action_del)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main = Main()
+    def action_equal(self):
+
+        # get the label text
+        equation = self.label.text()
+
+        try:
+            # getting the ans
+            ans = eval(equation)
+
+            # setting text to the label
+            self.label.setText(str(ans))
+
+        except:
+            # setting text to the label
+            self.label.setText("Wrong Input")
+
+    def action_plus(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + " + ")
+
+    def action_minus(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + " - ")
+
+    def action_div(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + " / ")
+
+    def action_mul(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + " * ")
+
+    def action_point(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + ".")
+
+    def action0(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "0")
+
+    def action1(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "1")
+
+    def action2(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "2")
+
+    def action3(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "3")
+
+    def action4(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "4")
+
+    def action5(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "5")
+
+    def action6(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "6")
+
+    def action7(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "7")
+
+    def action8(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "8")
+
+    def action9(self):
+        # appending label text
+        text = self.label.text()
+        self.label.setText(text + "9")
+
+    def action_clear(self):
+        # clearing the label text
+        self.label.setText("")
+
+    def action_del(self):
+        # clearing a single digit
+        text = self.label.text()
+        print(text[:len(text)-1])
+        self.label.setText(text[:len(text)-1])
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
-    
